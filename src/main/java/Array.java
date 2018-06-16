6ca2130f4e53edd1c3ded2af037dc58ac7b0a546
Alkatar <wandering90@gmail.com>
2018-06-16 19:17:57 +0300

    Поиск слова максимальной длины в массиве

---
+++ src/main/java/Array.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class Array {
+    public static void main(String[]args){
+        System.out.println("Введите размер массива ");
+        Scanner a = new Scanner(System.in);
+        int arrSize = a.nextInt();
+        String mas []=new String[arrSize];
+        Scanner in = new Scanner(System.in);
+        for (int i=0; i<arrSize; i++){
+            System.out.println("Введите следующую строку ");
+            mas[i]=in.nextLine();
+        }
+
+        int max=mas[0].length();
+        int maxi=0;
+        for (int i=0; i<arrSize; i++){
+            if (mas[i].length()>max){
+                max=mas[i].length();
+                maxi=i;
+            }
+        }
+        System.out.println("Самое большое слово в массиве это '" + mas[maxi]+ "'. Оно состоит из " + max + " букв");
+
+    }
+}
